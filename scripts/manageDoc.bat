d7e17ccbfece631853cdc331a9905861cb07803d
Bibhas Bhattacharya <bibhas2@gmail.com>
2014-03-14 15:10:01 -0400

    Enhance windows script.

--- scripts/manageDoc.bat
+++ scripts/manageDoc.bat
@@ -1,3 +1,6 @@
 @echo off
-set CLASSPATH=couchtools.jar;gson-1.7.1.jar;couchbase-client-1.2.0.jar;spymemcached-2.10.0.jar;httpcore-4.1.1.jar;httpcore-nio-4.1.1.jar;jettison-1.1.jar;netty-3.5.5.Final.jar;commons-codec-1.5.jar
+set script=%0
+for %%F in (%script%) do set dirname=%%~dpF
+
+set CLASSPATH=%dirname%couchtools.jar;%dirname%gson-1.7.1.jar;%dirname%couchbase-client-1.2.0.jar;%dirname%spymemcached-2.10.0.jar;%dirname%httpcore-4.1.1.jar;%dirname%httpcore-nio-4.1.1.jar;%dirname%jettison-1.1.jar;%dirname%netty-3.5.5.Final.jar;%dirname%commons-codec-1.5.jar
 java com.mobiarch.tools.ManageDoc %*
